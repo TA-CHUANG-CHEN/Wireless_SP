9b2bbea943147f419f26acd9fa8c5715ee2bbadd
TA-CHUANG <bignoodlepottage@gmail.com>
2020-07-17 09:11:25 +0800

    test

---
+++ js/dop-hover.js
@@ -0,0 +1,7 @@
+const product1 = document.querySelector('.product1')
+product1.addEventListener('mouseover',()=>{
+    const factory_auto = document.querySelector('factory-auto')
+    factory_auto.innerHTML =`{
+        
+    }`
+})
