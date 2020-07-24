7c246257b0b4404f813bcc939a6b8c22a4091820
TA-CHUANG <bignoodlepottage@gmail.com>
2020-07-25 07:08:49 +0800

    test

--- js/ov-click.js
+++ js/ov-click.js
@@ -1,11 +1,11 @@
-const proto = document.querySelector('.proto')
+const proto_1 = document.querySelector('.proto-1')
 const ov2_para4 = document.querySelector('.ov2-para4')
 const ov2_1 =document.querySelector('.ov2-1')
 document.addEventListener('click',(e)=>{
   if(!!ov2_1 && e.target == ov2_1){
-    proto.classList.remove('proto-display')
+    proto_1.classList.remove('proto-display')
   }
   if(e.target !== ov2_1){
-    proto.classList.add('proto-display')
+    proto_1.classList.add('proto-display')
   }
 })  
