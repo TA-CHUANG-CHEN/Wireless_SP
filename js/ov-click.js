d89006b10df360d19fae6912d44f106a90825ffd
TA-CHUANG <bignoodlepottage@gmail.com>
2020-07-27 08:19:12 +0800

    res_button

--- js/ov-click.js
+++ js/ov-click.js
@@ -1,5 +1,5 @@
 //click over-view  7 list
-const ovall = function (){
+const ov_all = function (){
   document.addEventListener('click',(e)=>{
     for(let i =1 ; i<8 ;i++){
     let proto_all =document.querySelector(`.proto-${i}`)
@@ -13,22 +13,15 @@ const ovall = function (){
     }
   })
 }()
-const factoryall = function(){
-  let item_1_1 = document.querySelector('.item-1-1')
-  let item_1_2 = document.querySelector('.item-1-2')
-  let item_2_1 = document.querySelector('.item-2-1')
-  let item_2_2 = document.querySelector('.item-2-2')
-  let item_2_3 = document.querySelector('.item-2-3')
-  let item_2_4 = document.querySelector('.item-2-4')
-  let item_3_1 = document.querySelector('.item-3-1')
-  let item_all = [item_1_1,item_1_2, item_2_1,item_2_2,item_2_3,item_2_4 ,item_3_1]
-  document.addEventListener('mouseover',(e)=>{
-    for(let i=0; i<7 ;i++){
-      let factory_item_all = document.querySelector(`.factory-item-${i+1}`)
-      if(!!e.target.closest(factory_item_all) ){
-        console.log('fuck')
-        //item_all[i].classList.remove('items-display')
-      }
+const products_all = function(){
+  const product_1 = document.getElementById('products-1')
+  const product_2 = document.getElementById('products-2')
+  const product_3 = document.getElementById('products-3')
+  document.addEventListener('click',(e)=>{
+    if(e.target == document.getElementById('factory-item-1')|| e.target == document.getElementById('factory-item-2')){
+      alert('fuck')
+      console.log(e.target)
     }
   })
+
 }()
